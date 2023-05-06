8523d999c36c5f5d02c0cb6447494b1909615230
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-06 11:47:13 +0000

    ストロングパラメータ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
-  # GET /users/:id
+  # GET /users/:id GETリクエストが/users/:idに来た時
   def show
     @user = User.find(params[:id])
   end
-
+  # GET /users/new GETリクエストが/users/newに来た時
   def new
+    @user = User.new
+  end
+  # POST /users(+ params)
+  def create
+    # (user + given params).save
+    # User.create(params[:user])
+    User.create(user_params)
+    # => name, email, pass/confirmation
+  end
+  
+  private#ここより上はやろうと思えば外側のコントローラからもアクセスできる。ここより下はこのファイルの中でしかアクセスできない。
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
