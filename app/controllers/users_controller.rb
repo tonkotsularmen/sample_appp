97caadd6435167d985c700188ba86f9a571058cb
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-07 01:16:21 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,20 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #Success
-    else 
+      # GET "/users/#{@user.id}"
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1) => /users/1
+    else
       #Failure
       render 'new'
-    end 
+    end
   end
-  
+
   private#ここより上はやろうと思えば外側のコントローラからもアクセスできる。ここより下はこのファイルの中でしかアクセスできない。
-  
+
   def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
