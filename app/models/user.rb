fe95bae9fbfdf603bb0789dd823fe58746d6bb26
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-14 13:59:19 +0000

    Finish 5/14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,19 @@ class User < ApplicationRecord
   # Default => class_name: "Micropost"
   # Default => foreign_key: "user_id",
   # => "#{Model name}s"
-  has_many :active_relationships,  class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                    dependent: :destroy
-  has_many :following,                through: :active_relationships, 
-                                       source: :followed
+  has_many :active_relationships,  class_name:  "Relationship",
+                                  foreign_key:  "follower_id",
+                                    dependent:  :destroy
+                                    
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key:  "followed_id",
+                                    dependent:  :destroy
+                                   
+  has_many :following,                through:  :active_relationships, 
+                                       source:  :followed
+                                       
+  has_many :followers,                through:  :passive_relationships, 
+                                       source:  :follower
   
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -90,6 +98,21 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
     # ("user_id = ?", self.id), ("user_id = ? ? ?", self.id,2,3)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
